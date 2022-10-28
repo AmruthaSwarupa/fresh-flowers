15709abe3891921d4c0f376ef8d004079f4e2c6d
Amruthaswarupa <amruthaswarupa286@gmail.com>
2022-10-28 11:38:23 +0530

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'freshflower'`, () => {
+  it(`should have as title 'flowershop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('freshflower');
+    expect(app.title).toEqual('flowershop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('freshflower app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('flowershop app is running!');
   });
 });
