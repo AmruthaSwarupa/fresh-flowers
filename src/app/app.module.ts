5f4b3bb410f80eedf42b412ab1930bd8e4c20f82
Amruthaswarupa <amruthaswarupa286@gmail.com>
2022-10-29 10:01:48 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
+import { ReviewComponent } from './review/review.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProductComponent,
+    ReviewComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
